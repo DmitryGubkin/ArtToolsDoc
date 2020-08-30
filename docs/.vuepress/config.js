93cb4ff986fb02dfdb2de399719215566bb1fafb
Dmitry <stalkerportal1@gmail.com>
2020-08-31 01:36:52 +0300

    !U add dark theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,9 @@ module.exports = {
     base: '/ArtToolsDoc/', 
     description: 'my tools',
     title: 'Art Tools by Dmitry Gubkin',
+    theme: 'default-prefers-color-scheme',
     themeConfig: {
+        defaultTheme: 'dark',
         nav:[
             {text: 'test', link: '/'}
         ],
